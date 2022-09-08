161cecdc02d083c73b2fecccd96dc6774b2a5b7e
localpharma <localpharma00@gmail.com>
2022-09-08 19:34:29 +0000

    built the individual product page

--- src/Pages/Productpage/Productpage.jsx
+++ src/Pages/Productpage/Productpage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import classes from "./Productpage.module.css";
 import { useParams } from "react-router-dom";
@@ -25,8 +26,6 @@ const Productpage = () => {
       } catch (error) {
         alert(error.message);
       }
-
-      console.log(selectedProduct);
     };
 
     fetchProduct(id);
